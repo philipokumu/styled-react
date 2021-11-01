77f951b54d39106deb774a72361b65144c2aa0eb
Philip Okumu <philipo.okumu@gmail.com>
2021-11-01 12:42:53 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import { Container } from "./Styles/Container.styled";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./Components/GlobalStyles";
+import content from "./content";
+import Card from "./Components/Card";
+import Footer from "./Components/Footer";
 
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => {
+            return <Card key={index} item={item} />;
+          })}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
   );
 }
 
